82b119b2d4bb3a229b1da58b32ae0c8500b6ac8c
g8y3e <valentine.pavchuk@ironsrc.com>
2016-07-08 14:29:53 +0300

    fixed travis

--- backoff/GBackoff/GBackoff/Properties/AssemblyInfo.cs
+++ backoff/GBackoff/GBackoff/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("valentine.pavchuk")]
+[assembly: AssemblyCopyright ("g8y3e")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
