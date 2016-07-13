3fc1e55940c27caae42e0e7852f9076dc3f69720
g8y3e <valentine.pavchuk@ironsrc.com>
2016-07-13 07:46:08 +0300

    added test project

---
+++ backoff/Test1/Test1/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test1 {
+    [TestFixture()]
+    public class Test {
+        [Test()]
+        public void TestCase() {
+        }
+
+        [Test()]
+        public void TestCase2() {
+            Assert.False(true);
+        }
+    }
+}
+
